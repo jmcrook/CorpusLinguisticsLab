739280d4b8175c3111b6f5e3fd46e7dd94fc68b6
Jackson Crook <jmcrook@umass.edu>
2015-04-24 00:29:26 -0400

    got the calculation figured out for a first order model

--- NGramModelPCCalc.py
+++ NGramModelPCCalc.py
@@ -44,11 +44,34 @@ print sum([(MDProb.prob(c) ** 2) for c in set(MobyDick)])
 #### Bigram model
 
 
-def bigram_g(r, book):
+def bigram_g(book):
 
-    bigram_dist = ConditionalProbDist(ConditionalFreqDist(bigrams(book)))
+    v = sorted(set(book))
+    word_dist = MLEProbDist(FreqDist(book))
+    bigram_dist = ConditionalProbDist(ConditionalFreqDist(bigrams(MobyDick)), MLEProbDist)
 
-    r_1 = sum([bigram_dist[C].prob(C) for C in set(book)])
+    r_1 = sum([bigram_dist[C].prob(C) * word_dist.prob(C) for C in v])
+
+    r_2 = 0
+    
+    for w in v:
+        for b in v:
+            r_2 += bigram_dist[b].prob(w) * bigram_dist[w].prob(b) * word_dist.prob(w)
+
+    r_3 = 0
+
+    for w in v:
+        for b in v:
+            for d in v:
+                r_3 += bigram_dist[d].prob(w) * bigram_dist[b].prob(d) * bigram_dist[w].prob(b) * word_dist.prob(w)
+
+    print r_1, r_2, r_3
+
+bigram_g(MobyDick)
+
+
+
+
 
 
 
